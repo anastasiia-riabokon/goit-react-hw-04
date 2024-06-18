fe2048d5bedf5dce5244bfeff73ab3e54a51cca8
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-06-18 19:30:17 +0300

    feat: add styles search bar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,6 +1,7 @@
 import {useForm} from "react-hook-form";
 import css from "./SearchBar.module.css";
 import toast from "react-hot-toast";
+import {CiSearch} from "react-icons/ci";
 
 const SearchBar = ({setQuery}) => {
   const {register, handleSubmit} = useForm();
@@ -12,16 +13,20 @@ const SearchBar = ({setQuery}) => {
     setQuery(data.query);
   };
   return (
-    <header>
-      <form onSubmit={handleSubmit(onSubmit)}>
+    <header className="bg-slate-600 p-4">
+      <form onSubmit={handleSubmit(onSubmit)} className="flex justify-center items-center gap-1">
+        <CiSearch color="white" size={32} />
         <input
           type="search"
           {...register("query")}
-          placeholder="Search..."
+          placeholder="Search images and photos"
           autoComplete="off"
           autoFocus
+          className="bg-transparent text-white py-1 px-2 w-2/4 outline-none font-play tracking-wide text-sm h-8 border-0 border-b border-white"
         />
-        <button type="submit">Search</button>
+        <button type="submit" className="text-white font-play tracking-wide text-sm">
+          Search
+        </button>
       </form>
     </header>
   );
