60e97344a87825b4278cf974dbe59489509cd3ce
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-06-19 15:17:08 +0300

    feat: styles load more

--- src/components/LoadMore/LoadMore.jsx
+++ src/components/LoadMore/LoadMore.jsx
@@ -3,7 +3,12 @@ import css from "./LoadMore.module.css";
 const LoadMore = ({onClick}) => {
   return (
     <>
-      <button onClick={onClick}>Load More</button>
+      <button
+        onClick={onClick}
+        className="block mx-auto mt-9 rounded-md bg-slate-600 hover:bg-slate-600/80 transition-colors ease-in-out duration-300 text-white p-2 font-play"
+      >
+        Load More
+      </button>
     </>
   );
 };
